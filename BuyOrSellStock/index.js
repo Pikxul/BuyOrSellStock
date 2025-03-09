ee77be576519c5f0b7dce65e0f1852a91c596312
Mrityunjoy <134674188+Pikxul@users.noreply.github.com>
2025-03-09 19:07:18 +0530

    index.js

--- BuyOrSellStock/index.js
+++ BuyOrSellStock/index.js
@@ -14,14 +14,36 @@
  * @param {number[]} prices
  * @return {number}
  */
+// function maxProfit(prices) {
+//     let maxProfit = 0;
+
+//     for (let i = 1; i < prices.length; i++) {
+//         if (prices[i] > prices[i - 1]) {
+//             maxProfit += prices[i] - prices[i - 1];
+//         }
+//     }
+
+//     return maxProfit;
+// }
+
+
+
 function maxProfit(prices) {
     let maxProfit = 0;
+    let actions = [];
 
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] > prices[i - 1]) {
             maxProfit += prices[i] - prices[i - 1];
+            actions.push({ action: 'Buy', day: i - 1, price: prices[i - 1] });
+            actions.push({ action: 'Sell', day: i, price: prices[i] });
         }
     }
 
+    console.log("Actions:", actions);
     return maxProfit;
 }
+
+// // Example usage:
+// const prices = [7, 1, 5, 3, 6, 4];
+// console.log("Max Profit:", maxProfit(prices));
